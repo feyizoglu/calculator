c6c35e88cd42a97a4901bd51fd347ab30a0e3191
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-03-02 23:43:00 +0300

    Creating Calculator Class

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// class Calculator {
+//   constructor(lastText, currentText) {
+//     this.lastText = lastText;
+//     this.currentText = currentText;
+//     this.clear();
+//   }
+
+//   clear() {
+//     this.currentOperand = "";
+//     this.previousOperand = "";
+//     this.operation = undefined;
+//   }
+
+//   delete() {}
+
+//   chooseOperation(operation) {}
+
+//   chooseNumber(number) {
+//     this.currentOperand = number;
+//   }
+//   updateDisplay() {
+//     this.currentText.innerText = this.currentOperand;
+//   }
+//   compute() {}
+// }
+
 const numberButtons = document.querySelectorAll(".number");
 const operationButtons = document.querySelectorAll(".operation");
 const eqButton = document.querySelector(".equals");
@@ -6,21 +32,15 @@ const deleteButton = document.querySelector(".delete");
 const lastText = document.querySelector(".last");
 const currentText = document.querySelector(".current");
 
-class Calculator {
-  constructor(lastText, currentText) {
-    this.lastText = lastText;
-    this.currentText = currentText;
-  }
+// const calculator = new Calculator(lastText, currentText);
 
-  clear() {
-    this.c;
-  }
+// numberButtons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     calculator.chooseNumber(button.innerText);
+//     calculator.updateDisplay();
+//   });
+// });
 
-  delete() {}
-
-  chooseOperation(operation) {}
-
-  chooseNumber(number) {}
-  updateDisplay() {}
-  compute() {}
-}
+// numberButtons.forEach((button) => {
+//   console.log(button.innerText);
+// });
