66a6fef69e5fde2040bedde52eb7f51f50470370
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-03-03 22:38:26 +0300

    Some Updates

--- script.js
+++ script.js
@@ -13,15 +13,29 @@ class Calculator {
 
   delete() {}
 
-  chooseOperation(operation) {}
+  chooseOperation(operation) {
+    if (this.currentOperand === "") return;
+    if (this.previousOperand !== "") {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
-  chooseNumber(number) {
-    this.currentOperand = number;
+  addNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
   }
   updateDisplay() {
     this.currentText.innerText = this.currentOperand;
+    this.lastText.innerText = this.previousOperand;
   }
-  compute() {}
+  compute() {
+    let computation;
+    const prev = parseFloat(this.previousOperand);
+    const current = parseFloat(this.currentOperand);
+  }
 }
 
 const numberButtons = document.querySelectorAll(".number");
@@ -29,19 +43,29 @@ const operationButtons = document.querySelectorAll(".operation");
 const eqButton = document.querySelector(".equals");
 const clearButton = document.querySelector(".clear");
 const deleteButton = document.querySelector(".delete");
-let lastText = document.querySelector("#last");
-const currentText = document.querySelector(".current");
+const lastText = document.querySelector("#last");
+const currentText = document.querySelector("#current");
 const calculator = new Calculator(lastText, currentText);
 
 numberButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    calculator.chooseNumber(button.innerHTML);
+    calculator.addNumber(button.innerText);
     calculator.updateDisplay();
   });
 });
 
-numberButtons.forEach((button) => {
+operationButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operationButtons.forEach((button) => {
   console.log(button.innerText);
 });
 
-console.log(lastText);
+eqButton.addEventListener("click", (button) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
