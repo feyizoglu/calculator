9d36921bc82e3c2b8284dbcb8b8c53e53b81b20b
Tolgahan Feyizoğlu <feyizoglutolgahan@hotmail.com>
2023-03-05 22:55:22 +0300

    Adding Usage of KeyCodes

--- script.js
+++ script.js
@@ -128,3 +128,44 @@ deleteButton.addEventListener("click", (button) => {
   calculator.delete();
   calculator.updateDisplay();
 });
+
+function keyCodes(event) {
+  if (event.keyCode > 95 && event.keyCode < 106) {
+    calculator.addNumber(event.keyCode - 96);
+    calculator.updateDisplay();
+  }
+}
+
+// let body = document.body;
+
+// let keyName = document.createElement("div");
+// let keyCode = document.createElement("div");
+
+// body.appendChild(keyName);
+// body.appendChild(keyCode);
+
+// body.style.fontFamily = "sans-serif";
+// // body.style.height = "100%";
+// body.style.display = "flex";
+// body.style.flexWrap = "wrap";
+
+// keyName.innerHTML = "<h1>Press any keyboard key</h1>";
+// keyName.style.height = "100%";
+// keyName.style.width = "100%";
+// keyName.style.border = "solid #ccc1c0";
+// keyName.style.boxShadow = "0 0 0 3px #9399a3";
+// keyName.style.textAlign = "center";
+// keyName.style.margin = "25% 25% 0 25%";
+
+// keyCode.innerHTML = "";
+
+// body.addEventListener("keydown", (e) => {
+//   if (e.which == 32){
+//     keyName.innerHTML = `<h1>You pressed <span>Space</span></h1>`;
+//   }else {
+//     keyName.innerHTML = `<h1>You pressed <span>${e.key}</span></h1>`;
+//   }
+//   let span1 = document.querySelector("span");
+//   keyCode.innerHTML = `<h1> ${e.which}</h1>`;
+
+// });
